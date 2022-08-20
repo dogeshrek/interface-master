2fa5b78d964db56a758eb7d0f2acde5bbb56f251
dogeshrek <111210935+dogeshrek@users.noreply.github.com>
2022-08-19 20:14:09 -0600

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,7 +1,6 @@
 // the Baguette Default token list lives here
-//export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/dogeshrek/dogeshrektokenlist/main/tokenLists/dogeShrekTokenList.json'
 
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/cyberminter/dogeshrek-tokens-list/main/tokenLists/dogeShrekTokenList.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/dogeshrek/dogeshrektokenlist/main/tokenLists/dogeShrekTokenList.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
